a982a791b7d4c3576bbecfaac0e6af3dfdd3f909
NadSafwat <nadine.hkm@aucegypt.edu>
2023-07-16 16:53:51 +0300

    created cache simulator

---
+++ simCache.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+enum cacheResType
+{
+    MISS = 0,
+    HIT = 1
+};
+
+struct CacheLine
+{
+    bool valid;
+    int Tag;
+};
+
+struct MemAddr
+{
+    int Tag;
+    int Index;
+    int Offset;
+};
+
+cacheResType simCache(MemAddr addr, vector<vector<CacheLine>> &cache, vector<int> &replaceCount);
+
+int main()
+{
+}
+
+//{{s0,s1,s2,...},{s0,s1,s3,...}} 2-way set associative would look like this
+cacheResType simCache(MemAddr addr, vector<vector<CacheLine>> &cache, vector<int> &replaceCount)
+{
+    int numWays = cache.size();
+    for (int i = 0; i < numWays; i++)
+    {
+        if (!(cache[i][addr.Index].valid))
+        {
+            // Coldstart MISS
+            cache[i][addr.Index].Tag == addr.Tag;
+            cache[i][addr.Index].valid == true;
+            return MISS;
+        }
+        if (cache[i][addr.Index].Tag == addr.Tag)
+            return HIT;
+    }
+
+    // Capacity MISS
+    cache[replaceCount[addr.Index]][addr.Index].Tag = addr.Tag;
+    replaceCount[addr.Index] = ++replaceCount[addr.Index] % numWays;
+    return MISS;
+}
